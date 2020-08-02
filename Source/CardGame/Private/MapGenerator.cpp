92bbe0b7e105c64fd6ac7a5b329496f9b20f75d9
sekulseb <sekulseb@gmail.com>
2020-08-02 12:18:55 +0200

    another changes

--- Source/CardGame/Private/MapGenerator.cpp
+++ Source/CardGame/Private/MapGenerator.cpp
@@ -1,27 +1,97 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "MapGenerator.h"
+#include "Components/StaticMeshComponent.h"
+#include "UObject/ConstructorHelpers.h"
+#include "Materials/MaterialInstanceDynamic.h"
 
 // Sets default values
 AMapGenerator::AMapGenerator()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
+	Root = CreateDefaultSubobject<USceneComponent>(TEXT("root"));
+	RootComponent = Root;
 
+	GetStaticAssets();
+	GenerateMapCords();
+	CreateStaticMeshes();
+}
+
+void AMapGenerator::PostInitializeComponents() {
+	Super::PostInitializeComponents();
+	SetDynamicMaterial(Texture);
 }
 
 // Called when the game starts or when spawned
 void AMapGenerator::BeginPlay()
 {
 	Super::BeginPlay();
-	
+}
+
+void AMapGenerator::PostLoad() {
+	Super::PostLoad();	
 }
 
 // Called every frame
 void AMapGenerator::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
 }
 
+
+// MAP METHODS
+
+void AMapGenerator::GenerateMapCords() 
+{
+	float offsetX = 0;
+	for (int row = 0; row < ROWS_AMOUNT; row++) {
+		for (int column = 0; column < COLUMNS_AMOUNT; column++) {
+			MapCords[row][column].X = (float)column * (HEX_DIMENSION_X + HEX_TILE_GAP);
+			MapCords[row][column].Y = (float)row * (Y_OFFSET + HEX_TILE_GAP);
+			MapCords[row][column].Z = 0;
+			if (row % 2) {
+				MapCords[row][column].X += X_OFFSET;
+			}
+		}
+	}
+}
+
+void AMapGenerator::GetStaticAssets() {
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> GetAssetHexTileMesh(TEXT("StaticMesh'/Game/map/hex-tile.hex-tile'"));
+	static ConstructorHelpers::FObjectFinder<UTexture> GetAssetHexTileTexture(TEXT("Texture2D'/Game/map/texture-hex-tile-1.texture-hex-tile-1'"));
+	static ConstructorHelpers::FObjectFinder<UMaterial> GetAssetHexTileMaterial(TEXT("Material'/Game/map/hex-material.hex-material'"));
+	if (GetAssetHexTileMesh.Succeeded()) {
+		StaticMesh = GetAssetHexTileMesh.Object;
+	}
+	if (GetAssetHexTileTexture.Succeeded()) {
+		Texture = GetAssetHexTileTexture.Object;
+	}
+	if (GetAssetHexTileMaterial.Succeeded()) {
+		Material = GetAssetHexTileMaterial.Object;
+	}
+}
+
+void AMapGenerator::CreateStaticMeshes() {
+	int index = 0;
+	for (int row = 0; row < ROWS_AMOUNT; row++)
+	{
+		for (int col = 0; col < COLUMNS_AMOUNT; col++)
+		{
+			FName meshName = MakeUniqueObjectName(this, AMapGenerator::StaticClass(), TEXT("Point"));
+			UStaticMeshComponent* meshComponent = CreateDefaultSubobject<UStaticMeshComponent>(meshName);
+			HexTiles[index++] = meshComponent;
+			meshComponent->AttachToComponent(Root, FAttachmentTransformRules::KeepWorldTransform);			
+			meshComponent->SetStaticMesh(StaticMesh);
+			meshComponent->SetMaterial(0, Material);
+			meshComponent->SetRelativeLocation(MapCords[row][col]);
+			meshComponent->SetWorldScale3D(FVector(1.f));			
+		}
+	}
+}
+
+void AMapGenerator::SetDynamicMaterial(UTexture* materialTexture) {
+	DynamicMaterial = HexTiles[0]->CreateAndSetMaterialInstanceDynamic(0);	
+	HexTiles[0]->SetMaterial(0, DynamicMaterial);
+	DynamicMaterial->SetTextureParameterValue(TEXT("MyTexture"), materialTexture);
+}
--- Source/CardGame/Public/MapGenerator.h
+++ Source/CardGame/Public/MapGenerator.h
@@ -4,16 +4,53 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include "Components/StaticMeshComponent.h"
+#include <string>
+#include "Math.h"
 #include "MapGenerator.generated.h"
+
+using namespace std;
+
+const int COLUMNS_AMOUNT = 10;
+const int ROWS_AMOUNT = 10;
+
+const int HEX_TILE_SIDE_LENGTH = 1;
+const float HEX_TILE_GAP = 1; // todo - fix this - gaps are not equal
+
+const float HEX_Y = HEX_TILE_SIDE_LENGTH * 100;
+const float HEX_DIMENSION_Y = HEX_Y * 2;
+
+const float HEX_X = ((HEX_TILE_SIDE_LENGTH * sqrt(3)) / 2) * 100;  // (a * sqrt(3)) / 2
+const float HEX_DIMENSION_X = HEX_X * 2;
+
+const float Y_OFFSET = HEX_Y * 1.5;
+const float X_OFFSET = HEX_X;
 
 UCLASS()
 class CARDGAME_API AMapGenerator : public AActor
 {
 	GENERATED_BODY()
-	
+
 public:	
 	// Sets default values for this actor's properties
 	AMapGenerator();
+
+	UPROPERTY()
+	USceneComponent* Root;
+	UPROPERTY(EditAnywhere)
+	UMaterial* Material;
+	UPROPERTY(EditAnywhere)
+	UMaterialInstanceDynamic* DynamicMaterial;
+	UPROPERTY(EditAnywhere)
+	UTexture* Texture;
+	UPROPERTY(EditAnywhere)
+	UStaticMesh* StaticMesh;
+	UPROPERTY(EditAnywhere)
+	UStaticMeshComponent* HexTiles[ROWS_AMOUNT * COLUMNS_AMOUNT];
+
+	FVector MapCords[ROWS_AMOUNT][COLUMNS_AMOUNT];
+
+private:
 
 protected:
 	// Called when the game starts or when spawned
@@ -22,5 +59,17 @@ protected:
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
+
+	virtual void PostInitializeComponents() override;
+
+	virtual void PostLoad() override;
+
+	virtual void GenerateMapCords();
+
+	virtual void CreateStaticMeshes();
+
+	virtual void GetStaticAssets();
+
+	virtual void SetDynamicMaterial(UTexture* materialTexture);
 
 };

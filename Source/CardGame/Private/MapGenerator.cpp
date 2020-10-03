5f735c849c062f57277683d83e4912b8da0c6319
sekulseb <sekulseb@gmail.com>
2020-10-03 19:50:37 +0200

    updated generator

--- Source/CardGame/Private/MapGenerator.cpp
+++ Source/CardGame/Private/MapGenerator.cpp
@@ -7,6 +7,20 @@ AMapGenerator::AMapGenerator()
 	PrimaryActorTick.bCanEverTick = false;
 	Root = CreateDefaultSubobject<USceneComponent>(TEXT("root"));
 	RootComponent = Root;
+
+	// load assets textures
+
+	// load assets material
+
+	// load assets mesh
+
+	// generate cords
+
+	// generate noise
+
+	// create and bind componentns to root scene
+
+	// apply dynamic material base on noise
 }
 
 void AMapGenerator::PostInitializeComponents() {
@@ -28,3 +42,19 @@ void AMapGenerator::Tick(float DeltaTime)
 	Super::Tick(DeltaTime);
 }
 
+UStaticMeshComponent* AMapGenerator::CreateStaticMeshComponent(FVector Cords) {
+	FName MeshName = MakeUniqueObjectName(this, AMapGenerator::StaticClass(), TEXT("Point"));
+	UStaticMeshComponent* meshComponent = CreateDefaultSubobject<UStaticMeshComponent>(MeshName);
+	meshComponent->AttachToComponent(Root, FAttachmentTransformRules::KeepWorldTransform);
+	meshComponent->SetStaticMesh(HexTileStaticMesh);
+	meshComponent->SetMaterial(0, HexTileMaterial);
+	meshComponent->SetRelativeLocation(Cords);
+	meshComponent->SetWorldScale3D(FVector(1.f));
+	return meshComponent;
+}
+
+void AMapGenerator::SetDynamicMaterial(UStaticMeshComponent* meshComponent, UTexture* materialTexture) {
+	HexTileDynamicMaterial = meshComponent->CreateAndSetMaterialInstanceDynamic(0);
+	meshComponent->SetMaterial(0, HexTileDynamicMaterial);
+	HexTileDynamicMaterial->SetTextureParameterValue("MapTexture", materialTexture);
+}
--- Source/CardGame/Public/MapGenerator.h
+++ Source/CardGame/Public/MapGenerator.h
@@ -1,7 +1,10 @@
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
 #include "Math.h"
-
+#include "MapConsts.h"
+#include "Components/StaticMeshComponent.h"
+#include "UObject/ConstructorHelpers.h"
+#include "Materials/MaterialInstanceDynamic.h"
 #include "MapGenerator.generated.h"
 
 #pragma once
@@ -15,6 +18,17 @@ class CARDGAME_API AMapGenerator : public AActor
 {
 	GENERATED_BODY()
 
+
+	UMaterialInstanceDynamic* HexTileDynamicMaterial;
+
+	UMaterial* HexTileMaterial;
+	UStaticMesh* HexTileStaticMesh;
+
+	UStaticMeshComponent* HexTiles[ROWS_AMOUNT][COLUMNS_AMOUNT];
+
+	virtual UStaticMeshComponent* CreateStaticMeshComponent(FVector Cords);
+	virtual void SetDynamicMaterial(UStaticMeshComponent* meshComponent, UTexture* materialTexture);
+
 public:
 	// Sets default values for this actor's properties
 	AMapGenerator();
@@ -22,15 +36,15 @@ public:
 	UPROPERTY()
 		USceneComponent* Root;
 
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-
-public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
 	virtual void PostInitializeComponents() override;
 
 	virtual void PostLoad() override;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
 };
